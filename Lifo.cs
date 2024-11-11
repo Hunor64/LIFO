9381603bdd568e741a6374c65d9a9b8d4896e1d3
hunor64 <nhunor06@gmail.com>
2024-11-11 08:36:53 +0100

    Add project files.

---
+++ Lifo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFO
+{
+    internal class Lifo
+    {
+        public List<int> stack;
+        public Lifo()
+        {
+            stack = new List<int>();
+        }
+        public void Push(int value)
+        {
+            stack.Add(value);
+        }
+        public void Pop()
+        {
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("Stack is empty!");
+            }
+            else
+            {
+                stack.RemoveAt(stack.Count - 1);
+            }
+        }
+        public int Get()
+        {
+            if (stack.Count == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                int value = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+                return value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using LIFO;
+
+Lifo lifo = new Lifo();
+for (int i = 0; i < 5; i++)
+{
+    lifo.Push(i);
+}
+Console.WriteLine(lifo.Get());
+
+foreach (int i in lifo.stack)
+{
+    Console.WriteLine(i);
+}
